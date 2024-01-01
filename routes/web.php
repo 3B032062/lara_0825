8a1431f1aa8191eb280b235650a469c1fef275fb
3B032062 <3B032062@gm.student.ncut.edu.tw>
2024-01-02 05:53:55 +0800

    使用條件式

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,18 @@ Route::get('/', function () {
     echo '內容：'.$post->content.'<br>';
     dd($post);
     */
-    
+
+    /*all方法
     $posts = Post::all();
     foreach ($posts as $post){
         echo '編號：'.$post->id.'<br>';
         echo '標題：'.$post->title.'<br>';
         echo '內容：'.$post->content.'<br>';
         echo '張貼時間'.$post->created_at.'<br>';
-    }
+       };
+    */
 
+
+    $posts = Post::where('id','<',10)->orderBy('id','DESC')->get();
+    dd($posts);
 });
