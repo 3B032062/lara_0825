43221f01bf8878c12791a3840b2b617de7c9a573
3B032062 <3B032062@gm.student.ncut.edu.tw>
2024-01-02 06:10:33 +0800

    取得model物件 單一筆貼文(條件式)

--- routes/web.php
+++ routes/web.php
@@ -79,10 +79,13 @@ Route::get('/', function () {
     dd($featurePosts);
     */
 
-
+    /*
     $fourthPost=Post::find(4);
     dd($fourthPost);
+    */
 
+    $lastPost = Post::orderBy('id','DESC')->first();
+    dd($lastPost);
 
 
 
